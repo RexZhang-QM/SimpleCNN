089708bb5017c89f458f053298787688b6a1a1a2
RexZhang-QM <zhangshaorun@gmail.com>
2024-10-01 19:19:24 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -13,6 +13,7 @@ def simple_model(num_classes=10, pretrained=False, **kwargs):
     """
     if pretrained:
         # 如果需要加载预训练的模型权重
+        print("直接加载模型权重...")
         model_url = 'https://raw.githubusercontent.com/RexZhang-QM/SimpleCNN/model_trained.pth'
         model_weights = requests.get(model_url).content
         model = SimpleCNN(num_classes=num_classes)
@@ -20,6 +21,7 @@ def simple_model(num_classes=10, pretrained=False, **kwargs):
         model.eval()
     else:
         # 如果不需要加载预训练的模型权重，即创建一个新的模型实例
+        print("不加载预训练的模型权重，创建一个新的模型实例...")
         model = SimpleCNN(num_classes=num_classes)
 
     return model
