06fa1f255b5c982a5d4966f973dce24fd40d70d3
RexZhang-QM <zhangshaorun@gmail.com>
2024-10-01 15:56:01 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,7 @@
 dependencies = ['torch']
 
-# 定义加载 SimpleCNN 模型的函数
+from model import SimpleCNN  # 确保你正确导入 SimpleCNN 类
+
 def simple_model(num_classes=10, **kwargs):
     """加载 SimpleCNN 模型."""
     model = SimpleCNN(num_classes=num_classes)
