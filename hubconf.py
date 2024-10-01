ee906464c41836a86376040c87bccf6c87961781
RexZhang-QM <zhangshaorun@gmail.com>
2024-10-01 22:34:41 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,21 +1,16 @@
-dependencies = ['torch', 'requests']
-
-import requests
 import torch
+import requests
 from io import BytesIO
 from SimpleCNN_model_for_hub import SimpleCNN
 
-def simple_model(num_classes=10, **kwargs):
-    """直接加载预训练的 SimpleCNN 模型权重。
+# 确保输出类别数与预训练权重匹配
+num_classes = 10
 
-    Args:
-    - num_classes (int): 输出类别数。
-    """
-    print("直接加载模型权重...")
-    model_url = 'https://raw.githubusercontent.com/RexZhang-QM/SimpleCNN/master/model_trained.pth'
-    response = requests.get(model_url)
-    model_weights = BytesIO(response.content)
-    model = SimpleCNN(num_classes=num_classes)
-    model.load_state_dict(torch.load(model_weights, map_location=torch.device('cpu')))
-    model.eval()
-    return model
+model = SimpleCNN(num_classes=num_classes)
+model.eval()
+
+# 直接加载预训练模型权重
+model_url = 'https://raw.githubusercontent.com/RexZhang-QM/SimpleCNN/master/model_trained.pth'
+response = requests.get(model_url)
+model_weights = BytesIO(response.content)
+model.load_state_dict(torch.load(model_weights, map_location=torch.device('cpu')))
